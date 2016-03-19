593cdde98cb60ec7177125d331807b28bf2d3f0a
Yousef Balout <yousefjb@outlook.com>
2016-03-19 21:55:45 +0200

    passenger display at random time

--- js/new.js
+++ js/new.js
@@ -1,5 +1,6 @@
 var imagesArray = ['pink.png', 'purple.png', 'red.png', 'green.png', 'yellow.png', 'blue.png'];
 var countFloor = 0;//to check elevator in which floor is
+var gameover = false;
 function startTimer(duration, display) {
     var timer = duration, seconds;
     setInterval(function () {
@@ -12,18 +13,27 @@ function startTimer(duration, display) {
         if (--timer < 0) {
             alert('Game Over');
             location.reload();
+			gameover = true;
         }
     }, 1000);
 }
+
+var counter = 500;
+var interval = setInterval(randomTimeImageDisplayer, counter);
+var randomTimeImageDisplayer = function(){
+	console.log("displayImage please");
+	displayImage();
+    clearInterval(interval);
+    counter = Math.floor((Math.random() * 1700) + 500);
+    interval = setInterval(randomTimeImageDisplayer, counter);
+}
+
 window.onload = function () {
     var Minute = 60, display = document.querySelector('#time');
     startTimer(Minute, display);
-    
-    var myVar = setInterval(myTimer, 5000);
-	function myTimer() {
-    	displayImage();
-	}
+	randomTimeImageDisplayer();
 };
+
 function displayImage(){
 	var randomnumber = Math.floor((Math.random() * 6));
 	var bomb = "floor" + randomnumber;
