f71eed7814af84a0cf48e824df5166a183248c13
Imad Daifallah <Imad Daifallah>
2016-03-30 17:12:35 +0200

    after designing floors

--- js/new.js
+++ js/new.js
@@ -15,17 +15,29 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
+var counter = 500;
+var interval = setInterval(randomTimeImageDisplayer, counter);
+var randomTimeImageDisplayer = function(){	
+	displayImage();
+    clearInterval(interval);
+    counter = Math.floor((Math.random() * 1700) + 500);
+    interval = setInterval(randomTimeImageDisplayer, counter);
+}
+
 window.onload = function () {
     var Minute = 60, display = document.querySelector('#time');
     startTimer(Minute, display);
-    
-    var myVar = setInterval(myTimer, 5000);
-	function myTimer() {
-    	displayImage();
-	}
+	randomTimeImageDisplayer();
+    var randomTarget = Math.floor((Math.random() * 15)) + 15;
+    console.log(randomTarget);
+    $("#target").html('Target: ' + randomTarget);
 };
+
 function displayImage(){
 	var randomnumber = Math.floor((Math.random() * 6));
+    if(randomnumber == 0){
+        randomnumber++;
+    }
 	var bomb = "floor" + randomnumber;
 	var num = Math.floor((Math.random() * 6));
 	if (randomnumber == num+1) {
