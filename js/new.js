b66640db0f06a87e9bc3e98b0eaabe9e893a3789
Yousef Balout <yousefjb@outlook.com>
2016-03-30 18:02:26 +0300

    merge of all branches

--- js/new.js
+++ js/new.js
@@ -17,7 +17,6 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
-
 var counter = 500;
 var interval = setInterval(randomTimeImageDisplayer, counter);
 var randomTimeImageDisplayer = function(){	
@@ -31,10 +30,16 @@ window.onload = function () {
     var Minute = 60, display = document.querySelector('#time');
     startTimer(Minute, display);
 	randomTimeImageDisplayer();
+    var randomTarget = Math.floor((Math.random() * 15)) + 15;
+    console.log(randomTarget);
+    $("#target").html('Target: ' + randomTarget);
 };
 
 function displayImage(){
 	var randomnumber = Math.floor((Math.random() * 6));
+    if(randomnumber == 0){
+        randomnumber++;
+    }
 	var bomb = "floor" + randomnumber;
 	var num = Math.floor((Math.random() * 6));
 	if (randomnumber == num+1) {
